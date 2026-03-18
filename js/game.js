9ef41bb7b0b22be75b564d919b0e6d2d8ff5ac60
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-03-18 18:33:54 +0100

    El pollo loco start

--- js/game.js
+++ js/game.js
@@ -4,14 +4,109 @@ let character = new Image();
 
 
 function init() {
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
+   
 
-character.onload = function () {
+    character.src = "img/2_character_pepe/2_walk/W-21.png";
+
     ctx.drawImage(character, 20, 20, 50, 150);
-};
-character.src = "img/2_character_pepe/2_walk/W-21.png";
-
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
