137b3088d88c44c0214d283d474f935a2c1d897c
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-03-19 17:44:25 +0100

    Add loadImage method to MovableObject and initialize images in Character and Chicken classes

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,12 @@
 class Character extends MovableObject {
    
 
+constructor () { 
+  super().loadImage('img/2_character_pepe/1_walk/W-21.png');
+
+  
+}
+
       jump () {
 
       }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,6 +4,11 @@ class MovableObject {
      img;
 
 
+loadImage (path) {
+     this.img = new Image();
+     this.img.src = path;
+}
+
       moveRight() {
         console.log('Moving right');
      } 
