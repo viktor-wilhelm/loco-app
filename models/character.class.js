bd2b144ef5b8679e0b413cc539c70a531256c631
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-03-29 12:01:46 +0200

    Update movement logic to correctly set character direction during movement

--- models/character.class.js
+++ models/character.class.js
@@ -39,6 +39,7 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.moveRight();
+                this.otherDirection = false;
             }
 
             if (this.world.keyboard.LEFT && this.x > 0) {
