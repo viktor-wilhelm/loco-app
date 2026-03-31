090ebc373a01db7754ca8787f165851392e7a07e
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-03-31 19:37:57 +0200

    Increase energy loss on collision with enemies from 2 to 5

--- models/world.class.js
+++ models/world.class.js
@@ -23,7 +23,7 @@ class World {
         setInterval(() => {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
-                    this.character.energy -= 2;
+                    this.character.energy -= 5;
                     console.log('Collision with Character', 'energy', this.character.energy);
                 }
             });
