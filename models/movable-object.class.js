faca383e6ce13664c83ce1fde154b8e88f5892ab
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-04-01 11:30:28 +0200

    Add character death and hurt animations; implement hit logic in MovableObject

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -11,7 +11,7 @@ class MovableObject {
      speedY = 0;
      acceleration = 2.5;
      energy = 100;
-
+     lastHit = 0;
 
      applayGrravity() {
           setInterval(() => {
@@ -54,6 +54,25 @@ class MovableObject {
               this.y < mo.y + mo.height;
      }
 
+     hit() {
+          this.energy -= 5;
+          if (this.energy < 0) {
+               this.energy = 0;
+          } else {
+               this.lastHit = new Date().getTime();
+          }
+     }
+
+     isHurt() {
+          let timepassed = new Date().getTime() - this.lastHit; // Differenz in ms
+          timepassed = timepassed / 1000; // Differenz in s
+          return timepassed < 1;
+     }
+
+     isDead() {
+          return this.energy == 0;
+     }
+
      /**
       * 
       * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
@@ -67,7 +86,7 @@ class MovableObject {
      }
 
      playAnimation(images) {
-          let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1, Rest 1
+          let i = this.currentImage % images.length; // let i = 7 % 6; => 1, Rest 1
           let path = images[i];
           this.img = this.imageCache[path];
           this.currentImage++;
--- models/world.class.js
+++ models/world.class.js
@@ -23,8 +23,7 @@ class World {
         setInterval(() => {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
-                    this.character.energy -= 5;
-                    console.log('Collision with Character', 'energy', this.character.energy);
+                    this.character.hit();
                 }
             });
         }, 200);
