a7edeae5b16bd544cef5481daaf906fddda2eae4
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-04-01 20:20:14 +0200

    Move status bar images to a new assets.js file; update index.html to include the new script

--- models/world.class.js
+++ models/world.class.js
@@ -1,30 +1,3 @@
-const IMAGES_HEALTH = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png', // 0
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png' // 5 
-];
-
-const IMAGES_COIN = [
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png', // 0
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png' // 5   
-];
-
-const IMAGES_BOTTLE = [
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
-];
-
 class World {
     character = new Character();
     level = level1;
