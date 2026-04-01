afaab5f6da527e55a73221cd30cc404f0261e6fb
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-04-01 20:05:17 +0200

    Refactor StatusBar class to accept dynamic images; update World class to instantiate multiple StatusBar objects for health, coins, and bottles

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,22 +1,14 @@
 class StatusBar extends DrawableObject {
 
-  IMAGES = [
-    'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png', // 0
-    'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-    'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-    'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-    'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-    'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png' // 5
-  ];
-
   percentage = 100;
 
-  constructor() {
+  constructor(y, images) {
     super();
-    this.loadImages(this.IMAGES);
+    this.loadImages(images);
+    this.images = images;
     this.setPercentage(100);
     this.x = 10;
-    this.y = 0  ;
+    this.y = y;
     this.width = 200;
     this.height = 55;
   }
@@ -24,7 +16,7 @@ class StatusBar extends DrawableObject {
   // setPercentage(50); 
   setPercentage(percentage) {
     this.percentage = percentage; // => 0 ... 5
-    let path = this.IMAGES[this.resolveImageIndex()];
+    let path = this.images[this.resolveImageIndex()];
     this.img = this.imageCache[path];
   }
 
--- models/world.class.js
+++ models/world.class.js
@@ -1,3 +1,30 @@
+const IMAGES_HEALTH = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png', // 0
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png' // 5 
+];
+
+const IMAGES_COIN = [
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png', // 0
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png' // 5   
+];
+
+const IMAGES_BOTTLE = [
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
+];
+
 class World {
     character = new Character();
     level = level1;
@@ -5,7 +32,9 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
-    statusBar = new StatusBar();
+    statusBar = new StatusBar(-10, IMAGES_HEALTH);
+    coinBar = new StatusBar(35, IMAGES_COIN);
+    bottleBar = new StatusBar(80, IMAGES_BOTTLE);
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -42,6 +71,8 @@ class World {
         this.ctx.translate(-this.camera_x, 0); // Back
         // -------- Space for fixed objects --------
         this.addToMap(this.statusBar);
+        this.addToMap(this.coinBar);
+        this.addToMap(this.bottleBar);
         this.ctx.translate(this.camera_x, 0); // Forwards
 
 
