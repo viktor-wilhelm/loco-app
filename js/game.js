ad0189a7185d625dece84cfbe8ca5c76d1642c89
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-04-08 08:43:03 +0200

    refactor: streamline keyboard event handling and improve code readability

--- js/game.js
+++ js/game.js
@@ -2,159 +2,22 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-
 function init() {
-    canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
-
-
-
-    console.log('My Character is', world.character);
-
+  canvas = document.getElementById("canvas");
+  world = new World(canvas, keyboard);
 }
 
-window.addEventListener('keydown', (e) => {
-   
-    if (e.keyCode == '39') {
-        keyboard.RIGHT = true;
-    }
-
-    if (e.keyCode == '37') {
-        keyboard.LEFT = true;
-    }
-
-    if (e.keyCode == '38') {
-        keyboard.UP = true;
-    }
-
-    if (e.keyCode == '40') {
-        keyboard.DOWN = true;
-    }
-
-    if (e.keyCode == '32') {
-        keyboard.SPACE = true;
-    }
-
-    if (e.keyCode == '68') {
-        keyboard.D = true;
-    }
-});
-
-window.addEventListener('keyup', (e) => {
-    if (e.keyCode == '39') {
-        keyboard.RIGHT = false;
-    }
-
-    if (e.keyCode == '37') {
-        keyboard.LEFT = false;
-    }
-
-    if (e.keyCode == '38') {
-        keyboard.UP = false;
-    }
-
-    if (e.keyCode == '40') {
-        keyboard.DOWN = false;
-    }
-
-    if (e.keyCode == '32') {
-        keyboard.SPACE = false;
-    }
-
-    if (e.keyCode == '68') {
-        keyboard.D = false;
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function handleKey(e, isPressed) {
+  const keyMap = {
+    ArrowRight: "RIGHT",
+    ArrowLeft: "LEFT",
+    ArrowUp: "UP",
+    ArrowDown: "DOWN",
+    " ": "SPACE",
+    d: "D",
+  };
+  if (keyMap[e.key] !== undefined) keyboard[keyMap[e.key]] = isPressed;
+}
+
+window.addEventListener("keydown", (e) => handleKey(e, true));
+window.addEventListener("keyup", (e) => handleKey(e, false));
