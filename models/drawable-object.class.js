f9f59e776ec800d27b0ae557a42a136bdfcd25ca
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-04-09 12:07:13 +0200

    refactor: comment out drawFrame method logic for future use

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -18,19 +18,19 @@ class DrawableObject {
   }
 
   drawFrame(ctx) {
-    if (this instanceof Character || this instanceof Chicken) {
-      ctx.beginPath();
-      ctx.lineWidth = '1';
-      ctx.strokeStyle = 'blue';
-      ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.stroke();
-    }
+    // if (this instanceof Character || this instanceof Chicken) {
+    //   ctx.beginPath();
+    //   ctx.lineWidth = '1';
+    //   ctx.strokeStyle = 'blue';
+    //   ctx.rect(this.x, this.y, this.width, this.height);
+    //   ctx.stroke();
+    // }
   }
 
   /**
- * 
- * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
- */
+   *
+   * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+   */
   loadImages(arr) {
     arr.forEach((path) => {
       let img = new Image();
